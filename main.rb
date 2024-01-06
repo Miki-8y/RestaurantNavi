a18fcd4ed3dc9c40c957def353a4da9967629ea3
Miki-8y <taichan.y.n.n@gmail.com>
2024-01-06 14:52:00 +0900

    ページング以外完成

--- main.rb
+++ main.rb
@@ -12,26 +12,12 @@ set :sessions,
     expire_after: 7200, 
     # 32バイトのランダムなシークレットキーを生成（rubyのバージョンが上がったため必須になった）
     secret:  SecureRandom.hex(32)
-    # secret: 'SPLAkyTwYa2L3Q2jwUjy'
-
-# 静的コンテンツ参照のためのパス設定
-# set :public, File.dirname(__FILE__) + '/public'
 
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection :development
 
 class Storeinfo < ActiveRecord::Base
 end
-
-ESCAPE_SS = {
-    '&' => '&amp;',
-    '<' => '&lt;',
-    '>' => '&gt;',
-    '"' => '&quot;',
-    "'" => '&#39;',
-}
-
-CHECK = ["<font","<h1>","<h2>","<h3>","<h4>","<h5>","<h6>","</font>","</h1>","</h2>","</h3>","</h4>","</h5>","</h6>"]
 
 before do
     response.headers['Access-Control-Allow-Origin'] = '*'
@@ -41,20 +27,22 @@ get '/' do
     redirect '/top'
 end
 
+post '/top' do
+    redirect '/top'
+end
+
+######################店舗検索ページ(topページ)####################
 get '/top' do
     Storeinfo.delete_all
     erb :'/RestaurantNavi/Top/index', :layout => :'/RestaurantNavi/Top/layout'
 end
 
 post '/detail' do
-    # Debug
-    # id = params[:r_id]
-    # puts(id)
     s = Storeinfo.new
 
     # Top画面の詳細をクリックした店舗情報のみをstoreinfoテーブルに保存
     s.id = params[:r_id]
-    s.name = params[:r_name]
+    s.name = params[:r_name] + '(' + params[:r_name_kana] + ')'
     s.logoimage_url = params[:r_logoimage_url]
     s.address = params[:r_address]
     s.access = params[:r_access]
@@ -69,27 +57,18 @@ post '/detail' do
     redirect '/detail'
 end
 
+######################店舗詳細ページ(detailページ)####################
 get '/detail' do
     @s = Storeinfo.where(id: $s_id)
-    erb :'/RestaurantNavi/Detail/index', :layout => :'/RestaurantNavi/Top/layout'
+    erb :'/RestaurantNavi/Detail/index', :layout => :'/RestaurantNavi/Detail/layout'
 end
 
 ######################プロフィールページ(Aboutページ)####################
 get '/about' do
     erb :'/About/about', :layout => :'/About/layout'
-    # if(session[:login_flag] == true)
-    #     erb :'/About/about', :layout => :'/About/layout'
-    # else
-    #     erb :'badrequest', :layout => :'/APP/layout'
-    # end
 end
 
 ######################連絡先ページ(Contactページ)####################
 get '/contact' do
     erb :'/Contact/contact', :layout => :'/Contact/layout'
-    # if(session[:login_flag] == true)
-    #     erb :'/Contact/contact', :layout => :'/Contact/layout'
-    # else
-    #     erb :'badrequest', :layout => :'/APP/layout'
-    # end
 end
